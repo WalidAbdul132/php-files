7d5591aba586ac45897da26ea199cac0f621ff46
WalidAbdul132 <abdulbuster132@gmail.com>
2023-04-10 15:56:28 +0400

    Create 6.php

---
+++ Excercise/6.php
@@ -0,0 +1,17 @@
+<?php
+    // Define a multidimensional array with employee data
+    $emp = array(
+      array(1, "David", 400000),
+      array(2, "John", 500000),
+      array(3, "Rahul", 300000)
+    );
+    
+    // Get the first element of the array
+    echo "Printing first element in an array</br>";
+    $first_element = $emp[0];
+    // Print the first element
+    print_r($first_element);
+
+
+
+?>
